3ffd5c01325256125f9bab5affd324e8f544ac32
cjy2103 <cjy2103@naver.com>
2022-01-11 03:36:41 +0900

    Kotlin ViewBinding 설명

---
+++ for blog function pratice/Kotlin/Binding/ViewBinding/app/src/main/java/com/example/viewbinding/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.viewbinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.viewbinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var mBinding : ActivityMainBinding? = null // 바인딩 객체 선언
+    private val binding get() = mBinding!! // null 체크 편의성 위해 바인딩 변수 재선언
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initBinding()
+
+        wordChagne()
+
+    }
+
+    /**
+     * @DESC:초기 바인딩
+     */
+    private fun initBinding(){
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    /**
+     * @DESC: 단어변경
+     */
+    private fun wordChagne(){
+        binding.btnChagne.setOnClickListener {
+            binding.tvWord.text = "단어변경"
+        }
+    }
+}
