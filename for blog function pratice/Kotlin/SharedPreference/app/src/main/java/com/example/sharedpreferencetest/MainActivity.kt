07a2d36846f48d0287ff396facf491599e59254a
cjy2103 <cjy2103@naver.com>
2022-01-25 00:55:45 +0900

    Kotlin SharedPreference 설명 완

--- for blog function pratice/Kotlin/SharedPreference/app/src/main/java/com/example/sharedpreferencetest/MainActivity.kt
+++ for blog function pratice/Kotlin/SharedPreference/app/src/main/java/com/example/sharedpreferencetest/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.sharedpreferencetest
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     private var mBinding : ActivityMainBinding? = null
     private val binding get() = mBinding!!
     private lateinit var mContext: Context
+    private lateinit var mActivity: Activity
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +23,8 @@ class MainActivity : AppCompatActivity() {
         initialize()
 
         moveSubActivity()
+        
+        loadData();
     }
 
     /**
@@ -36,6 +40,7 @@ class MainActivity : AppCompatActivity() {
      */
     private fun initialize(){
         mContext = this@MainActivity
+        mActivity = this@MainActivity
     }
 
     /**
@@ -45,6 +50,17 @@ class MainActivity : AppCompatActivity() {
         binding.btnMove.setOnClickListener {
             val intent = Intent(mContext, SubActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    /**
+     * @DESC: 데이터 로드
+     */
+    private fun loadData(){
+        binding.btnLoad.setOnClickListener{
+            val sharedPreferences = mActivity.getSharedPreferences("SaveData", MODE_PRIVATE)
+            val str = sharedPreferences.getString("Data","저장된 데이터 없음")
+            binding.tvWord.text = str
         }
     }
 }
