0f12bece921396169754f505cc62585942265d08
cjy2103 <cjy2103@naver.com>
2022-03-02 02:51:14 +0900

    Kotlin 폰트 변경 완

--- for blog function pratice/Kotlin/Design/FontApply/app/src/main/java/com/example/fontchange/MainActivity.kt
+++ for blog function pratice/Kotlin/Design/FontApply/app/src/main/java/com/example/fontchange/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.fontchange
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fontchange.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var mBinding : ActivityMainBinding? = null
+    private val binding get() = mBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        initBinding()
+
+        initFontSetting()
+    }
+
+    /**
+     * @DESC: 초기 바인딩
+     */
+    private fun initBinding(){
+        mBinding    = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    /**
+     * @DESC: 폰트세팅
+     */
+    private fun initFontSetting(){
+        val tfMapleBold = Typeface.createFromAsset(this.assets, "Maplestory Bold.ttf")
+        val tfMapleLight = Typeface.createFromAsset(this.assets, "Maplestory Light.ttf")
+
+        binding.tvMapleBold.typeface = tfMapleBold
+        binding.tvMapleLight.typeface = tfMapleLight
     }
 }
