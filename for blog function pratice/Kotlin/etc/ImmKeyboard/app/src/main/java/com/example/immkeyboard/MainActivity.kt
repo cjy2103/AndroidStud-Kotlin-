48e25714e8cab41af4d11508f1486c26d9d6bc59
jy.choi <cjy2103@naver.com>
2022-03-02 17:59:40 +0900

    IMM Keyboard 구현 완

---
+++ for blog function pratice/Kotlin/etc/ImmKeyboard/app/src/main/java/com/example/immkeyboard/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.immkeyboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.example.immkeyboard.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var mBinding : ActivityMainBinding? = null
+    private val binding get() = mBinding!!
+    private lateinit var imm : InputMethodManager
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initBinding()
+
+        initalize()
+
+        clickOpen()
+        
+        clickClose()
+    }
+
+    /**
+     * @DESC: 초기 바인딩
+     */
+    private fun initBinding(){
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun initalize(){
+        binding.edtInput.requestFocus()
+        imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+    }
+
+    /**
+     * @DESC: IMM 키보드 열기
+     */
+    private fun clickOpen(){
+        binding.btnOpen.setOnClickListener {
+            imm.showSoftInput(binding.edtInput,0)
+        }
+    }
+
+    /**
+     * @DESC: IMM 키보드 닫기
+     */
+    private fun clickClose(){
+        binding.btnClose.setOnClickListener {
+            imm.hideSoftInputFromWindow(binding.edtInput.windowToken,0)
+        }
+    }
+}
