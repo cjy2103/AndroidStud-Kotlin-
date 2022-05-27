030e099176dc1de1f05847cec2d0681615feb3f1
jy.choi <cjy2103@naver.com>
2022-05-27 15:35:12 +0900

    XML 폰트 적용 포스팅 완

--- for blog function pratice/Kotlin/etc/SplashActivity/app/src/main/java/com/example/splash/SplashActivity.kt
+++ for blog function pratice/Kotlin/etc/SplashActivity/app/src/main/java/com/example/splash/SplashActivity.kt
@@ -1,13 +1,48 @@
 package com.example.splash
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.bumptech.glide.Glide
+import com.example.splash.databinding.ActivitySplashBinding
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
 
+    private var mBinding : ActivitySplashBinding? = null
+    private val binding get() = mBinding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+
+
+        viewBinding()
+
+        imageSetting()
+
+        moveMain()
+    }
+
+    private fun viewBinding(){
+        mBinding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun imageSetting(){
+        Glide.with(this).load(R.drawable.overme).into(binding.ivSplash)
+    }
+
+    private fun moveMain() {
+        runOnUiThread {
+            Handler(Looper.myLooper()!!).postDelayed({
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }, 2000)
+        }
     }
 }
