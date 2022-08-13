1a7c6fa5faadc1f47e008dee19989d3ffe309b97
cjy2103 <cjy2103@naver.com>
2022-08-13 21:28:49 +0900

    코틀린 구독취소 페이지

--- for blog function pratice/Kotlin/etc/SubScribeCancel/app/src/main/java/com/example/subscribecancel/MainActivity.kt
+++ for blog function pratice/Kotlin/etc/SubScribeCancel/app/src/main/java/com/example/subscribecancel/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.example.subscribecancel
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.subscribecancel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var mBinding : ActivityMainBinding? = null
+    private val binding get() = mBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding()
+
+        btnAppSubscribe()
+        btnAllSubscribe()
+    }
+
+    private fun viewBinding(){
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun btnAppSubscribe() {
+        binding.btnAppSubscribe.setOnClickListener { v ->
+            val uri =
+                Uri.parse("https://play.google.com/store/account/subscriptions?sku=your-sub-product-id&package=your-app-package")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            intent.setPackage("com.android.vending")
+            startActivity(intent)
+        }
+    }
+
+    private fun btnAllSubscribe() {
+        binding.btnAllSubscribe.setOnClickListener { v ->
+            val uri =
+                Uri.parse("https://play.google.com/store/account/subscriptions")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            intent.setPackage("com.android.vending")
+            startActivity(intent)
+        }
     }
 }
