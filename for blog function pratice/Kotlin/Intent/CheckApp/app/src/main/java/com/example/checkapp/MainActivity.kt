f787c5679083d916d768a692b1e0e69e4f9cd3b8
jy.choi <cjy2103@naver.com>
2022-11-25 10:51:55 +0900

    앱체크 (코틀린) 포스팅 완

--- for blog function pratice/Kotlin/Intent/CheckApp/app/src/main/java/com/example/checkapp/MainActivity.kt
+++ for blog function pratice/Kotlin/Intent/CheckApp/app/src/main/java/com/example/checkapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.checkapp
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.checkapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -24,10 +27,36 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun btnUmaCheck(){
-        
+        binding.btnUmaCheck.setOnClickListener {
+            val packageName = "com.kakaogames.umamusume"
+            val intent = packageManager.getLaunchIntentForPackage(packageName)
+            if(intent != null){
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            } else {
+                try {
+                    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$packageName")))
+                } catch (e : android.content.ActivityNotFoundException){
+                    Toast.makeText(this,"플레이스토어 앱 존재하지않음",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 
     private fun btnPokemonCheck(){
-
+        binding.btnPokemonCheck.setOnClickListener {
+            val packageName = "com.nianticlabs.pokemongo"
+            val intent = packageManager.getLaunchIntentForPackage(packageName)
+            if(intent != null){
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            } else {
+                try {
+                    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$packageName")))
+                } catch (e : android.content.ActivityNotFoundException){
+                    Toast.makeText(this,"플레이스토어 앱 존재하지않음",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
