7df0f547d65dcc6c6a7cc0307b48dfe3f43fef98
jy.choi <cjy2103@naver.com>
2023-01-05 10:02:40 +0900

    코틀린 주소검색 포스팅 완

--- for blog function pratice/Kotlin/Intent/AddressSearchAPI(Daum)/app/src/main/java/com/example/addresssearch/SearchActivity.kt
+++ for blog function pratice/Kotlin/Intent/AddressSearchAPI(Daum)/app/src/main/java/com/example/addresssearch/SearchActivity.kt
@@ -1,7 +1,12 @@
 package com.example.addresssearch
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.view.KeyEvent
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
 import com.example.addresssearch.databinding.ActivitySearchBinding
 
 class SearchActivity : AppCompatActivity() {
@@ -12,10 +17,40 @@ class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding()
+        init()
     }
 
     private fun viewBinding(){
         mBinding = ActivitySearchBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    @SuppressLint("JavascriptInterface", "SetJavaScriptEnabled")
+    private fun init(){
+        binding.webView.settings.javaScriptEnabled = true
+        binding.webView.addJavascriptInterface(BridgeInterface(), "Android")
+        binding.webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView, url: String) {
+                binding.webView.loadUrl("javascript: sample2_execDaumPostcode();")
+            }
+        }
+        binding.webView.loadUrl("your_server_url")
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && binding.webView.canGoBack()) {
+            binding.webView.goBack()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
+    inner class BridgeInterface{
+        fun processDATA(data : String){
+            val intent = Intent()
+            intent.putExtra("data",data)
+            setResult(RESULT_OK, intent)
+            finish()
+        }
     }
 }
