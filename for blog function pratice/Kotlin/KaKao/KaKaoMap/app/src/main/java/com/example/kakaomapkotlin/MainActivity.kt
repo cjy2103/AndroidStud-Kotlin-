ea36a47af7a590b074f1300f1d1f7b2174baeb82
jy.choi <cjy2103@naver.com>
2023-02-12 20:54:46 +0900

    카카오맵 마커달기 포스팅 완

--- for blog function pratice/Kotlin/KaKao/KaKaoMap/app/src/main/java/com/example/kakaomapkotlin/MainActivity.kt
+++ for blog function pratice/Kotlin/KaKao/KaKaoMap/app/src/main/java/com/example/kakaomapkotlin/MainActivity.kt
@@ -15,8 +15,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         dataBinding()
-        val hashKey = HashKey()
-        hashKey.migrateSignatures(this)
+//        val hashKey = HashKey()
+//        hashKey.migrateSignatures(this)
     }
 
     private fun dataBinding(){
--- for blog function pratice/Kotlin/KaKao/KaKaoMap/app/src/main/java/com/example/kakaomapkotlin/vm/MapViewModel.kt
+++ for blog function pratice/Kotlin/KaKao/KaKaoMap/app/src/main/java/com/example/kakaomapkotlin/vm/MapViewModel.kt
@@ -3,6 +3,8 @@ package com.example.kakaomapkotlin.vm
 import android.content.Context
 import android.view.View
 import android.view.ViewGroup
+import net.daum.mf.map.api.MapPOIItem
+import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
 
 class MapViewModel {
@@ -13,5 +15,22 @@ class MapViewModel {
         mapView = MapView(context)
         mapViewContainer = view as ViewGroup
         view.addView(mapView)
+
+        markAdd()
+    }
+
+    fun markAdd(){
+        // 시작위치 변경
+        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(35.1399,129.0986), true)
+        // 마커달기
+        val markerPoint = MapPoint.mapPointWithGeoCoord(35.1399,129.0986)
+        val marker = MapPOIItem()
+        marker.itemName = "경성대학교"
+        marker.tag = 0
+        marker.mapPoint = markerPoint
+        marker.markerType = MapPOIItem.MarkerType.BluePin
+        marker.selectedMarkerType = MapPOIItem.MarkerType.RedPin
+
+        mapView.addPOIItem(marker)
     }
 }
