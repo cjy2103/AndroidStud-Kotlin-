6ba09d982fcc263a401c86863f62a0c81b186d9d
jy.choi <cjy2103@naver.com>
2023-02-21 21:25:51 +0900

    데이터 바인딩 포스팅 완

--- for blog function pratice/Kotlin/Binding/DataBinding/app/src/main/java/com/example/myapplication/vm/MainViewModel.kt
+++ for blog function pratice/Kotlin/Binding/DataBinding/app/src/main/java/com/example/myapplication/vm/MainViewModel.kt
@@ -1,4 +1,12 @@
 package com.example.myapplication.vm
 
+import android.content.Context
+import android.view.View
+import android.widget.Toast
+
 class MainViewModel {
+    fun showToast(view : View){
+        val context = view.context
+        Toast.makeText(context, "버튼 클릭됨",Toast.LENGTH_SHORT).show()
+    }
 }
