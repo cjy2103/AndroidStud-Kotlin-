cfd320baf04d70f75f9caf414f7492bb7d0022a9
jy.choi <cjy2103@naver.com>
2023-03-02 18:03:57 +0900

    저장

--- for blog function pratice/Kotlin/Design/FloatingButton/app/src/main/java/com/example/floatingbutton/MainActivity.kt
+++ for blog function pratice/Kotlin/Design/FloatingButton/app/src/main/java/com/example/floatingbutton/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.floatingbutton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.floatingbutton.databinding.ActivityMainBinding
+import com.example.floatingbutton.vm.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
-
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel = MainViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,6 +17,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun dataBinding(){
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
     }
 }
--- for blog function pratice/Kotlin/Design/FloatingButton/app/src/main/java/com/example/floatingbutton/vm/MainViewModel.kt
+++ for blog function pratice/Kotlin/Design/FloatingButton/app/src/main/java/com/example/floatingbutton/vm/MainViewModel.kt
@@ -2,14 +2,24 @@ package com.example.floatingbutton.vm
 
 import android.animation.ObjectAnimator
 import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
-import android.widget.ImageView
+import androidx.lifecycle.MutableLiveData
 import com.example.floatingbutton.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainViewModel {
+
+    private var fabJazzAnimation = MutableLiveData<Animation>()
+    get() = fabJazzAnimation
+
+    private var fabDiyapAnimation = MutableLiveData<Animation>()
+    get() = fabDiyapAnimation
+
+
     private lateinit var fabOpen : Animation
     private lateinit var fabClose : Animation
 
@@ -37,7 +47,16 @@ class MainViewModel {
     private fun animation(fabView: FloatingActionButton) {
         if(isFapOpen){
             ObjectAnimator.ofFloat(fabView, View.ROTATION, 45f,0f).start()
-
+            fabView.backgroundTintList = ColorStateList.valueOf(Color.parseColor("ACF6FF"))
+            fabJazzAnimation.value = fabClose
+            fabDiyapAnimation.value = fabClose
+            isFapOpen = false
+        } else {
+            ObjectAnimator.ofFloat(fabView, View.ROTATION, 0f, 45f).start()
+            fabView.backgroundTintList = ColorStateList.valueOf(Color.parseColor("41A9FB"))
+            fabJazzAnimation.value = fabOpen
+            fabDiyapAnimation.value = fabOpen
+            isFapOpen = true
         }
     }
 
