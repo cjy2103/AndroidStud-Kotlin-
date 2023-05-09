a20c744b6f649324bfb1eeaea6abf27575e7ffa6
jy.choi <cjy2103@naver.com>
2023-05-09 15:18:12 +0900

    Android BLE Classic 소켓 생성, 끊기

--- for blog function pratice/Kotlin/etc/BLE/app/src/main/java/com/example/ble/MainActivity.kt
+++ for blog function pratice/Kotlin/etc/BLE/app/src/main/java/com/example/ble/MainActivity.kt
@@ -1,19 +1,33 @@
 package com.example.ble
 
+import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothSocket
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.ble.databinding.ActivityMainBinding
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+import java.util.UUID
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "BLE Example"
 
     private var mBinding : ActivityMainBinding? = null
     private val binding get() = mBinding!!
 
     private lateinit var bluetoothAdapter: BluetoothAdapter
     private val deviceAddress = "00:00:00:00:00:00" // BLE 장치의 MAC 주소 (수정 하세요)
+
+    private lateinit var socket : BluetoothSocket
+    private var outputStream: OutputStream? = null
+    private var inputStream: InputStream? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +36,10 @@ class MainActivity : AppCompatActivity() {
         init()
 
         blePairing()
+
+        bleSocketCreate()
+
+        bleSocketClose()
     }
 
     private fun viewBinding(){
@@ -49,4 +67,49 @@ class MainActivity : AppCompatActivity() {
             e.printStackTrace()
         }
     }
+
+
+    private fun bleSocketCreate(){
+        binding.btnSocketCreate.setOnClickListener {
+            createSocket()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun createSocket(){
+        val pairedDevices = bluetoothAdapter.bondedDevices
+        var deviceToConnect: BluetoothDevice? = null
+
+        for(device in pairedDevices){
+            if(device.address == deviceAddress){
+                deviceToConnect = device
+                break
+            }
+        }
+
+        if(deviceToConnect == null) {
+            Log.e(TAG, "Device not found.")
+            return
+        }
+
+        // BluetoothSocket을 생성하고, 장치와 연결합니다.
+        val uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb")
+        val socket = deviceToConnect.createRfcommSocketToServiceRecord(uuid)
+        try {
+            socket.connect()
+            Log.i(TAG, "Socket connected.")
+            outputStream = socket.outputStream
+            inputStream = socket.inputStream
+        } catch (e: IOException) {
+            Log.e(TAG, "Error occurred while connecting to socket.", e)
+            return
+        }
+    }
+
+    private fun bleSocketClose(){
+        binding.btnSocketClose.setOnClickListener {
+            socket.close()
+        }
+    }
+
 }
