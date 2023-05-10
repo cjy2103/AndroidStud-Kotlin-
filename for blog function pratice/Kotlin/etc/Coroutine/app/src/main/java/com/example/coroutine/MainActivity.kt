6021116ead753c90c17cb7f62aa61ec3b56a46a0
jy.choi <cjy2103@naver.com>
2023-05-10 15:20:33 +0900

    코루틴 빌더 부분 사용

--- for blog function pratice/Kotlin/etc/Coroutine/app/src/main/java/com/example/coroutine/MainActivity.kt
+++ for blog function pratice/Kotlin/etc/Coroutine/app/src/main/java/com/example/coroutine/MainActivity.kt
@@ -2,10 +2,46 @@ package com.example.coroutine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.coroutine.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+
+    private var mBinding : ActivityMainBinding? = null
+    private val binding get() = mBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding()
+
+        executeTask()
+    }
+
+    private fun viewBinding(){
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun executeTask(){
+        binding.btnExecute.setOnClickListener {
+            val coroutineScope = CoroutineScope(Dispatchers.Main)
+
+            val backgroundWork = suspend {
+                delay(2000)
+                "Coroutine Work"
+            }
+
+            coroutineScope.launch(Dispatchers.IO){
+                val result = backgroundWork()
+                withContext(Dispatchers.Main){
+                    binding.tvTest.text = result
+                }
+            }
+        }
+
     }
 }
