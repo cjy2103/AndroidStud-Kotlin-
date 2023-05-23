c938ebffeb9d3d824c876eaa051d957a463986af
jy.choi <cjy2103@naver.com>
2023-05-23 17:34:17 +0900

    Flow 준비

--- for blog function pratice/Kotlin/DB/FlowRoom/app/src/main/java/com/example/flow/DataViewModel.kt
+++ for blog function pratice/Kotlin/DB/FlowRoom/app/src/main/java/com/example/flow/DataViewModel.kt
@@ -2,13 +2,41 @@ package com.example.flow
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.room.Room
 import com.example.flow.room.Data
 import com.example.flow.room.DataDao
+import com.example.flow.room.RoomDataBase
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class DataViewModel(application: Application) : AndroidViewModel(application) {
     private val dataDao : DataDao
     private val dataFlow : Flow<List<Data>>
 
+    private val _dataList = MutableStateFlow<List<Data>>(emptyList())
+    val dataList: StateFlow<List<Data>> get() = _dataList
+
+    init {
+        val database = Room.databaseBuilder(
+            application,
+            RoomDataBase::class.java,
+            "data_database"
+        ).build()
+
+        dataDao = database.dataDao()
+        dataFlow = dataDao.getAllData()
+        observeData()
+    }
+
+    private fun observeData() {
+        viewModelScope.launch {
+            dataFlow.collect { data ->
+                _dataList.emit(data)
+            }
+        }
+    }
     
 }
--- for blog function pratice/Kotlin/DB/FlowRoom/app/src/main/java/com/example/flow/room/RoomDataBase.kt
+++ for blog function pratice/Kotlin/DB/FlowRoom/app/src/main/java/com/example/flow/room/RoomDataBase.kt
@@ -1,8 +1,9 @@
 package com.example.flow.room
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 
 @Database(entities = [Data::class], version = 1)
-abstract class RoomDataBase {
+abstract class RoomDataBase : RoomDatabase(){
     abstract fun dataDao() : DataDao
 }
