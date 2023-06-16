0716d228ffe7dc016a658c6ce02aece5e65f0f8a
DESKTOP-JQ763B1\cjy2103 <cjy2103@naver.com>
2023-06-16 12:27:03 +0900

    프로그래스바 포스팅 완

--- for blog function pratice/Kotlin/Design/ProcessBar/app/src/main/java/com/example/processbar/MainActivity.kt
+++ for blog function pratice/Kotlin/Design/ProcessBar/app/src/main/java/com/example/processbar/MainActivity.kt
@@ -7,11 +7,18 @@ import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import com.example.processbar.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private var mBinding : ActivityMainBinding? = null
     private val binding get() = mBinding!!
+
+    private var progressJob: Job? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,14 +35,15 @@ class MainActivity : AppCompatActivity() {
 
     private fun clickStart(){
         binding.btnStart.setOnClickListener {
-            caseThread()
+//            caseThread()
+            caseCoroutine()
         }
     }
 
     @SuppressLint("SetTextI18n")
     private fun caseThread(){
         binding.tvState.text = "Loading"
-        val totalDuration = 11000 // 총 길이는 5초 (5000ms)
+        val totalDuration = 5000 // 총 길이는 5초 (5000ms)
         val interval = 100 // 게이지를 업데이트할 간격 (100ms)
 
         val maxProgress = 100
@@ -53,7 +61,7 @@ class MainActivity : AppCompatActivity() {
                 progress += progressIncrement
                 binding.progressBar.progress = progress.toInt()
 
-                var percent = (progress.toDouble() / maxProgress * 100).toInt()
+                var percent = (progress / maxProgress * 100).toInt()
                 if(percent > 100){
                     percent = 100
                 }
@@ -71,6 +79,39 @@ class MainActivity : AppCompatActivity() {
 // 애니메이션 시작
         handler.postDelayed(runnable, interval.toLong())
     }
+
+
+    @SuppressLint("SetTextI18n")
+    private fun caseCoroutine(){
+        binding.tvState.text = "Loading"
+        val totalDuration = 5000 // 총 길이는 5초 (5000ms)
+        val interval = 100 // 게이지를 업데이트할 간격 (100ms)
+        val maxProgress = 100
+
+        val progressIncrement = 100.0 / (totalDuration.toDouble() / interval.toDouble())
+
+        progressJob?.cancel() // 기존 진행 중인 작업 취소
+
+        progressJob = CoroutineScope(Dispatchers.Main).launch {
+            var progress = 0.0
+
+            while (progress < maxProgress) {
+                delay(interval.toLong())
+
+                progress += progressIncrement
+                binding.progressBar.progress = progress.toInt()
+
+                var percent = (progress / maxProgress * 100).toInt()
+                if (percent > 100) {
+                    percent = 100
+                }
+                binding.tvPercent.text = "$percent%"
+            }
+
+            binding.tvState.text = "Finish"
+        }
+    }
+
 
     @SuppressLint("SetTextI18n")
     private fun clickReset(){
