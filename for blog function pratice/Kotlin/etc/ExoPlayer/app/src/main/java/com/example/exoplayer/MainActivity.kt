def47921337a2c8d1dd8f3488ba143176f2571e2
DESKTOP-JQ763B1\cjy2103 <cjy2103@naver.com>
2023-06-26 18:01:04 +0900

    중간 저장

--- for blog function pratice/Kotlin/etc/ExoPlayer/app/src/main/java/com/example/exoplayer/MainActivity.kt
+++ for blog function pratice/Kotlin/etc/ExoPlayer/app/src/main/java/com/example/exoplayer/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.exoplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.exoplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var mBinding : ActivityMainBinding? = null
+    private val binding get() = mBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding()
+    }
+
+    private fun viewBinding(){
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
