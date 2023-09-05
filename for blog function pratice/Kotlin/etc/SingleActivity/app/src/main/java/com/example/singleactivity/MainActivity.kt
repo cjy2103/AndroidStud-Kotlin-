b847bf139356e2a2b6a8716ea5cdfb3a564e64bd
DESKTOP-JQ763B1\cjy2103 <cjy2103@naver.com>
2023-09-05 14:07:04 +0900

    중간 저장

--- for blog function pratice/Kotlin/etc/SingleActivity/app/src/main/java/com/example/singleactivity/MainActivity.kt
+++ for blog function pratice/Kotlin/etc/SingleActivity/app/src/main/java/com/example/singleactivity/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.singleactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.singleactivity.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var mBinding : ActivityMainBinding? = null
+    private val binding get() = mBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding()
+
+        init()
     }
+
+    private fun viewBinding(){
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun init(){
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+
+
+
+
+    }
+
+
 }
