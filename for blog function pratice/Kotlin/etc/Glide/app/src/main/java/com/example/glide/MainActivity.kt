7f5a9dce36eee1212472fed25400dc6430a6e09a
DESKTOP-JQ763B1\cjy2103 <cjy2103@naver.com>
2023-09-07 12:28:25 +0900

    no message

--- for blog function pratice/Kotlin/etc/Glide/app/src/main/java/com/example/glide/MainActivity.kt
+++ for blog function pratice/Kotlin/etc/Glide/app/src/main/java/com/example/glide/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.glide
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestBuilder
+import com.example.glide.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var mBinding : ActivityMainBinding? = null
+    private val binding get() = mBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Glide.with(this).load("https://via.placeholder.com/300.png").placeholder(R.drawable.test)
+            .dontAnimate()
+            .into(binding.imageView)
+//        val requestBuilder: RequestBuilder<Drawable> = Glide.with(this)
+//            .asDrawable().sizeMultiplier(0.1f)
+//
+//        Glide.with(this).load(R.drawable.fail).
+//        thumbnail(requestBuilder).into(binding.imageView)
+
     }
 }
