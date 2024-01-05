c308a235066f325a113c1bae9fa9207580a3272a
S120916\최재영 <cjy2103@naver.com>
2024-01-05 16:21:18 +0900

    안드로이드(Android) Kotlin - 리스트 내용을 원하는 형식으로 출력하기 준비중

--- for blog function pratice/Kotlin/etc/ListTextView/app/src/main/java/com/example/listtextview/MainActivity.kt
+++ for blog function pratice/Kotlin/etc/ListTextView/app/src/main/java/com/example/listtextview/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.listtextview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.listtextview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var mBinding : ActivityMainBinding? = null
+    private val binding get() = mBinding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.btnInfo.setOnClickListener {
+
+            val list = ArrayList<Person>()
+
+            list.add(Person("철수",21))
+            list.add(Person("영희",15))
+            list.add(Person("다비",26))
+
+            Log.v("뭔데?", list[0].name)
+
+            val persons = list.map {
+                val name = it.name
+                val age = it.age
+                "(Name : $name, age : $age)"
+            }
+            Log.v("뭔데", persons.toString())
+
+            binding.tvPerson.text = persons.joinToString ("\n")
+        }
+
     }
 }
