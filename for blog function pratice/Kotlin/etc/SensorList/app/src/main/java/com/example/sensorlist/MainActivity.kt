bbe42aa4cfbcef51c571be20ed71601986bd3586
S120916\최재영 <cjy2103@naver.com>
2024-01-05 15:08:19 +0900

    안드로이드 센서 목록 리스트 가져오기 포스팅 준비

--- for blog function pratice/Kotlin/etc/SensorList/app/src/main/java/com/example/sensorlist/MainActivity.kt
+++ for blog function pratice/Kotlin/etc/SensorList/app/src/main/java/com/example/sensorlist/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.sensorlist
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sensorlist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var mBinding : ActivityMainBinding? = null
+    private val binding get() = mBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSearch.setOnClickListener {
+            searchSensorList()
+        }
+    }
+
+    private fun searchSensorList(){
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL)
+
+        val sensors = sensorList.map {
+            val name = it.name
+            val type = it.type
+            "( Name : $name, Type: $type )"
+        }
+        binding.tvSensorList.text = sensors.joinToString ("\n")
     }
 }
